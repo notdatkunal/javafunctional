88379911736a4901da3e7e2ac5152e92e5574f88
Kunal Dabhane <kunaldabhane20012@gmail.com>
2023-05-01 22:49:20 +0530

    second

--- src/main/java/com/kunal/Main.java
+++ src/main/java/com/kunal/Main.java
@@ -1,5 +1,59 @@
 package com.kunal;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.kunal.Main.Gender.*;
+
 public class Main {
+    public static void main(String[] args) {
+        List<Person> people = List.of(
+            new Person("Kunal",MALE),
+            new Person("Akash",MALE),
+            new Person("Shruti",FEMALE),
+            new Person("Rutuja",FEMALE),
+            new Person("Akanksha",FEMALE)
+
+        );
+        //Imperative Approach
+        List<Person> females = new ArrayList<>();
+
+        for (Person person:people) {
+            if (FEMALE.equals(person.gender))
+                females.add(person);
+        }
+
+        for (Person person:females) {
+            System.out.println(person);
+        }
+
+        System.out.println("declarative approach");
+        //Declarative approach
+        people.stream().filter(person -> FEMALE.equals(person.gender)).collect(Collectors.toList()).forEach(System.out::println);
+
+
+
+    }
+    static class Person{
+        private final String name;
+        private final Gender gender;
+
+        Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+     enum Gender{
+        MALE,FEMALE
+    }
 
 }
